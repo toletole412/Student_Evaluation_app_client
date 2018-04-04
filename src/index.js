44491f890c53012126ef7dc1ea2068b587a82251
toletole412 <yoonjioh90@gmail.com>
2018-04-05 00:52:07 +0200

    install index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store} className='unscroll'>
+    <App className='unscroll'/>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
