f4e8be19691f6e47868bbc8f5c2cfbde789d0db5
toletole412 <yoonjioh90@gmail.com>
2018-04-05 00:51:56 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+
+import LoginPage from './containers/LoginPage'
+import Batches from './containers/Batches'
+import BatchDetail from './containers/BatchDetail'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className='App'>
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/batches" component={Batches} />
+          <Route exact path="/batches/:id" component={BatchDetail} />
+        </div>
+      </Router>
     );
   }
 }
