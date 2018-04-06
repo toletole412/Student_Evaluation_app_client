d727f81e9925a72adc98532a019650efff0260a5
toletole412 <yoonjioh90@gmail.com>
2018-04-06 11:50:52 +0200

    packages

--- src/containers/StudentEdit.js
+++
@@ -1,9 +0,0 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
-
-//create, edit, delete
-
-class StudentEdit extends PureComponent {
-  
-}
