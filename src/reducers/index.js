033d3c216b28effe5dd91e16d817eb19bcdcc06d
toletole412 <yoonjioh90@gmail.com>
2018-04-06 11:46:19 +0200

    add reducers in index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,22 @@ import login from './login'
 import currentTeacher from './currentTeacher'
 import batchesInfo from './batchesInfo'
 import batch from './batch'
+import evaluation from './evaluation'
+import student from './student'
+import question from './question'
+import redP from './redP'
+import yellowP from './yellowP'
+import greenP from './greenP'
 
 export default {
   login,
   currentTeacher,
   batchesInfo,
-  batch
+  batch,
+  evaluation,
+  student,
+  question,
+  redP,
+  greenP,
+  yellowP
 }
