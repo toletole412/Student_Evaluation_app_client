2b16d1c03fb3f64a0b4fb8228dcf348fa562b7cc
toletole412 <yoonjioh90@gmail.com>
2018-04-06 11:48:30 +0200

    Add EditEvaluation component

---
+++ src/containers/EditEvaluation.js
@@ -0,0 +1,56 @@
+import React, {PureComponent} from 'react'
+import { editEvaluation, fetchOneEvaluation, deleteEvaluation } from '../actions/students'
+import EditEvaluationForm from '../components/EditEvaluationForm'
+import AppBar from 'material-ui/AppBar';
+import { connect } from 'react-redux'
+
+
+
+const style = {
+  margin: 5,
+};
+
+
+class EditEvaluation extends PureComponent {
+
+  componentWillMount() {
+      this.props.fetchOneEvaluation(this.props.match.params.id)
+  }
+
+   editEvaluation = evaluation => {
+     this.props.editEvaluation(evaluation, this.props.match.params.id)
+   }
+
+   deleteEvaluation = () => {
+     this.props.deleteEvaluation(this.props.match.params.id)
+   }
+
+
+  render() {
+    const { evaluation } = this.props
+
+
+    return (
+        <div>
+        <AppBar
+          title="Evaluation"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+            <EditEvaluationForm onSubmit={this.editEvaluation}/>
+            <button
+                onClick={
+                  this.deleteEvaluation
+                }>
+                delete evalutions
+            </button>
+        </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  evaluation: state.evaluation,
+  student: state.student
+})
+
+export default connect(mapStateToProps, {editEvaluation, fetchOneEvaluation, deleteEvaluation} )(EditEvaluation)
