aedcc0a755ac9d6dcc0cf602ebd475f389d6a73d
toletole412 <yoonjioh90@gmail.com>
2018-04-06 11:41:59 +0200

    Add student actions

--- src/actions/students.js
+++ src/actions/students.js
@@ -1,5 +1,9 @@
 import * as request from 'superagent'
-import { FETCH_BATCHES, FETCH_BATCH, ADD_STUDENT, EDIT_STUDENT, DELETE_STUDENT } from './types'
+import { FETCH_BATCHES, FETCH_BATCH, CREATE_BATCH,
+  ADD_STUDENT, EDIT_STUDENT, DELETE_STUDENT,FETCH_STUDENT,
+  DELETE_EVALUATION, DO_EVALUATION,EDIT_EVALUATION, FETCH_EVALUATION,
+  ASK_A_QUESTION,  GET_RED, GET_YELLOW, GET_GREEN
+   } from './types'
 
 const baseUrl = 'http://localhost:4000'
 
@@ -27,9 +31,20 @@ export const fetchOneBatch = (id) => (dispatch) => {
     .catch(err => console.error(err))
 }
 
+export const createBatch = (batch) => (dispatch) => {
+  request
+    .post(`${baseUrl}/batches`)
+    .send(batch)
+    .then(response => dispatch({
+      type: CREATE_BATCH,
+      payload: response.body
+    }))
+}
+
+
 export const addStudent = (student, batchId) => (dispatch) => {
   request
-    .post(`${baseUrl}/students/${batchId}`)
+    .post(`${baseUrl}/${batchId}/student/new`)
     .send(student)
     .then(response => dispatch({
       type: ADD_STUDENT,
@@ -37,24 +52,124 @@ export const addStudent = (student, batchId) => (dispatch) => {
     }))
 }
 
-// export const login = (email, password) => (dispatch) =>
-//   request
-//     .post(`${baseUrl}/logins`)
-//     .send({email, password})
-//     .then(result => {
-//       dispatch({
-//         type: TEACHER_LOGIN_SUCCESS,
-//         payload: result.body
-//       })
-//     })
-//     .catch(err => {
-//       if (err.status === 400) {
-//         dispatch({
-//           type: TEACHER_LOGIN_FAILED,
-//           payload: err.response.body.message || 'Unknown error'
-//         })
-//       }
-//       else {
-//         console.error(err)
-//       }
-//     })
+export const editStudent = (student, studentId) => (dispatch) => {
+  request
+    .put(`${baseUrl}/students/${studentId}/edit`)
+    .send(student)
+    .then(response => dispatch({
+      type: EDIT_STUDENT,
+      payload: response.body
+    }))
+}
+
+export const deleteStudent = (studentId) => (dispatch) => {
+  request
+    .delete(`${baseUrl}/students/${studentId}/delete`)
+    .send(studentId)
+    .then(response => dispatch({
+      type: DELETE_STUDENT,
+      payload: response.body
+    }))
+}
+
+export const deleteEvaluation = (evaluationId) => (dispatch) => {
+  request
+    .delete(`${baseUrl}/evaluation/${evaluationId}/delete`)
+    .send(evaluationId)
+    .then(response => dispatch({
+      type: DELETE_EVALUATION,
+      payload: response.body
+    }))
+}
+
+export const editEvaluation = (evaluation, evaluationId) => (dispatch) => {
+  request
+    .put(`${baseUrl}/students/${evaluationId}/evaluation/edit`)
+    .send(evaluation)
+    .then(response => dispatch({
+      type: EDIT_EVALUATION,
+      payload: response.body
+    }))
+}
+
+export const doEvaluation = (evaluation, studentId) => (dispatch) => {
+  request
+    .post(`${baseUrl}/students/${studentId}/evaluation`)
+    .send(evaluation)
+    .then(response => dispatch({
+      type: DO_EVALUATION,
+      payload: response.body
+    }))
+}
+
+export const fetchOneEvaluation = (id) => (dispatch) => {
+  request
+    .get(`${baseUrl}/evaluation/${id}`)
+    .then(result => {
+      dispatch({
+        type: FETCH_EVALUATION,
+        payload: result.body
+      })
+    })
+    .catch(err => console.error(err))
+}
+
+export const fetchOneStudent = (id) => (dispatch) => {
+  request
+    .get(`${baseUrl}/students/${id}`)
+    .then(result => {
+      dispatch({
+        type: FETCH_STUDENT,
+        payload: result.body
+      })
+    })
+    .catch(err => console.error(err))
+}
+
+export const askQuestion = (batchId) => (dispatch) => {
+  request
+    .get(`${baseUrl}/batch/${batchId}/ask`)
+    .then(result => {
+      dispatch({
+        type: ASK_A_QUESTION,
+        payload: result.body
+      })
+    })
+    .catch(err => console.error(err))
+}
+
+export const getRedP = (batchId) => (dispatch) => {
+  request
+    .get(`${baseUrl}/${batchId}/redP`)
+    .then(result => {
+      dispatch({
+        type: GET_RED,
+        payload: result.body
+      })
+    })
+    .catch(err => console.error(err))
+}
+
+export const getGreenP = (batchId) => (dispatch) => {
+  request
+    .get(`${baseUrl}/${batchId}/greenP`)
+    .then(result => {
+      dispatch({
+        type: GET_GREEN,
+        payload: result.body
+      })
+    })
+    .catch(err => console.error(err))
+}
+
+export const getYellowP = (batchId) => (dispatch) => {
+  request
+    .get(`${baseUrl}/${batchId}/yellowP`)
+    .then(result => {
+      dispatch({
+        type: GET_YELLOW,
+        payload: result.body
+      })
+    })
+    .catch(err => console.error(err))
+}
