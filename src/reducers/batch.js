38897a0368aa51f29e2485fc464244d7e6ab279f
toletole412 <yoonjioh90@gmail.com>
2018-04-06 11:46:03 +0200

    add reducers

--- src/reducers/batch.js
+++ src/reducers/batch.js
@@ -1,11 +1,18 @@
-import { FETCH_BATCH, ADD_STUDENT } from '../actions/types'
+import { FETCH_BATCH, ADD_STUDENT, CREATE_BATCH , DELETE_STUDENT } from '../actions/types'
 
 export default (state = {}, { type, payload }) => {
   switch (type) {
     case FETCH_BATCH:
       return payload
 
+    case CREATE_BATCH:
+      return [...state, payload]
+
     case ADD_STUDENT:
+      return [...state, payload]
+
+  
+    case DELETE_STUDENT:
       return [...state, payload]
 
     default:
